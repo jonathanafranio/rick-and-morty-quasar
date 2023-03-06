0fdcedadcd61924e8a96ce147a8246f6fb236411
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-03-06 19:01:10 -0300

    feat: pagina 404

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+    <LayoutDefault>
+        <div
+            class="page-section__fullscreen text-white text-center q-pa-md flex flex-center"
+        >
+            <div>
+                <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+                <div class="text-h2" style="opacity: 0.4">
+                    Oops. Nada por aqui...
+                </div>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
-    </div>
-  </div>
+                <q-btn
+                    class="q-mt-xl"
+                    color="white"
+                    text-color="blue"
+                    unelevated
+                    to="/"
+                    label="Go Home"
+                    no-caps
+                />
+            </div>
+        </div>
+    </LayoutDefault>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent } from "vue";
+import LayoutDefault from "components/Layouts/LayoutDefault.vue";
 export default defineComponent({
-  name: 'ErrorNotFound'
-})
+    name: "ErrorNotFound",
+    components: {
+        LayoutDefault,
+    },
+});
 </script>
